46bbb13fc23f68edfcdea55070a0362caeb6bbb7
Amina Babić <ababic2@etf.unsa.ba>
2020-05-19 02:33:24 +0200

    Dodan User kao Identity

---
+++ eBay/Areas/Identity/Data/eBayUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace eBay.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the eBayUser class
+    public class eBayUser : IdentityUser
+    {
+
+    }
+}
--- eBay/Startup.cs
+++ eBay/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using eBay.Models;
+using eBay.Areas.Identity.Data;
 
 namespace eBay
 {
@@ -28,15 +29,18 @@ namespace eBay
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("Baza")));
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("Baza")));
             services.AddDbContext<EBayContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("Baza")));
+                    Configuration.GetConnectionString("eBayContextConnection")));
+            //services.AddDbContext<eBayContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("Baza")));
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            //services.AddDefaultIdentity<eBayUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<eBayContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
